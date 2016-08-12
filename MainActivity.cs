58f8b3f2f638c9d6edb9e58e86f5a7b04eafaacc
stevenseow <stevenseowmunhoe@hotmail.com>
2016-08-12 15:47:19 +1000

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Project1_StudyPlanner
+{
+	[Activity(Label = "Project1_StudyPlanner", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+ 		Button buttonAddNote;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			//Button add to another view 			buttonAddNote = FindViewById<Button>(Resource.Id.buttonAdd);   			if (buttonAddNote != null) 			{ 				buttonAddNote.Click += (sender, e) => 				{ 					StartActivity(typeof(NoteTakingActivity)); 				} ; 			} 
+		}
+	}
+}
+
+
---
+++ NoteTakingActivity.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Project1_StudyPlanner
+{
+	[Activity(Label = "NoteTakingActivity")]
+	public class NoteTakingActivity : Activity
+	{
+
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+		}
+	}
+}
+
